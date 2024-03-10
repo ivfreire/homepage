caed7b57d0c4dd441a2f413a4a5a4e805ccab5b6
Ícaro Freire <ivfreire@usp.br>
2024-03-10 06:10:24 -0300

    new

--- apps/cg_demo.js
+++ apps/cg_demo.js
@@ -3,24 +3,27 @@
 // Institute of Nuclear & Energy Research - IPEN/CNEN-SP
 
 // Globals
-width = window.innerWidth;
-height = window.innerWidth;
+size = 3
 
 const scene = new THREE.Scene();
 
 scene.background = new THREE.Color(0xeeeeee);
 
 // const camera = new THREE.PerspectiveCamera(75, 1.0, 0.1, 1000);
-const camera = new THREE.OrthographicCamera(3 / - 2, 3 / 2, 3 / 2, 3 / - 2, 1, 1000);
+const camera = new THREE.OrthographicCamera(size / - 2, size / 2, size / 2, size / - 2, 1, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-renderer.setSize(width, width);
+renderer.setSize(window.innerWidth, window.innerWidth);
 document.getElementById('cg_demo').appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({color: 0x282f72});
+const material = new THREE.MeshStandardMaterial({color: 0x282f72});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 0, 2);
+scene.add(pointLight);
 
 camera.position.z = 2;
 
@@ -28,6 +31,7 @@ function animate() {
 	requestAnimationFrame(animate);
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+
 	renderer.render(scene, camera);
 }
 
